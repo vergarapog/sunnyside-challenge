34146eaabb1543d011faa33808f1319f312bdc14
vergarapog <201801166@iacademy.edu.ph>
2021-07-28 18:10:17 +0800

    test

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -27,9 +27,9 @@ const Navbar = () => {
   });
 
   const transitions = useTransition(showMenu, {
-    from: { opacity: 0, transform: "translateY(+100%)" },
-    enter: { opacity: 1, transform: "translateY(0%)" },
-    leave: { opacity: 0, transform: "translateY(+100%)" },
+    from: { opacity: 0, transform: "translateX(+100%)" },
+    enter: { opacity: 1, transform: "translateX(0%)" },
+    leave: { opacity: 0, transform: "translateX(+100%)" },
   });
 
   // if (showMenu) {
